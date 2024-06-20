0c919db6e8b496d703e7e84c8034af69ba97f33a
Rasdenmartinez <libre93@live.com.mx>
2024-06-19 22:27:41 -0600

    new methods added post, get, update

--- ejercicio1-metodosCrud/src/main/java/ms/ejercicio1_metodosCrud/controller/DepartamentoController.java
+++ ejercicio1-metodosCrud/src/main/java/ms/ejercicio1_metodosCrud/controller/DepartamentoController.java
@@ -3,11 +3,9 @@ package ms.ejercicio1_metodosCrud.controller;
 import ms.ejercicio1_metodosCrud.entity.Departamento;
 import ms.ejercicio1_metodosCrud.service.impl.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,5 +17,15 @@ public class DepartamentoController {
     public Optional<Departamento> readById(@PathVariable Long id){
         return departamentoService.readById(id);
     }
+
+    @GetMapping("/departamento/todos")
+    public List<Departamento> readAll(){
+        return departamentoService.readAll();
+    }
+    @PostMapping("/departamento")
+    public Departamento create(@RequestBody Departamento departamento){
+        return departamentoService.create(departamento);
+    }
 }
+
 
