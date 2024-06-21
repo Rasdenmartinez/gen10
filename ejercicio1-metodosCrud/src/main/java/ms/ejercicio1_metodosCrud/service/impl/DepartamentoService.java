697309c790cb579f04d6ec8c9ab0bbb33f776104
Rasdenmartinez <libre93@live.com.mx>
2024-06-20 22:13:29 -0600

    add methods create and delete with remove logic

--- ejercicio1-metodosCrud/src/main/java/ms/ejercicio1_metodosCrud/service/impl/DepartamentoService.java
+++ ejercicio1-metodosCrud/src/main/java/ms/ejercicio1_metodosCrud/service/impl/DepartamentoService.java
@@ -1,5 +1,6 @@
 package ms.ejercicio1_metodosCrud.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import ms.ejercicio1_metodosCrud.entity.Departamento;
 import ms.ejercicio1_metodosCrud.repository.DepartamentoRepository;
 import ms.ejercicio1_metodosCrud.service.IDepartamentoService;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 @Service
+@Slf4j
 public class DepartamentoService implements IDepartamentoService {
     @Autowired
     DepartamentoRepository departamentoRepository;
@@ -21,7 +23,7 @@ public class DepartamentoService implements IDepartamentoService {
 
     @Override
     public List<Departamento> readAll() {
-        return departamentoRepository.findAll();
+        return departamentoRepository.findAll().stream().filter(s->s.getIsActive()).toList();
     }
 
     @Override
@@ -31,11 +33,29 @@ public class DepartamentoService implements IDepartamentoService {
 
     @Override
     public Departamento update(Departamento deparmento) {
-        return null;
+        return departamentoRepository.save(deparmento);
     }
 
     @Override
-    public void delete(Long id) {
+    public String delete(Departamento departamento) {
+        Optional<Departamento> departamentoOpt=departamentoRepository.findById(departamento.getId());
+        if (departamentoOpt.isPresent()){
+            Departamento departamentoBase=departamentoOpt.get();
+            departamentoBase.setIsActive(false);
+            try{
+                departamentoRepository.save(departamentoBase);
+                return "Departamento "+departamentoBase.getId()+" borrado correctamente";
+            }catch(Exception ex){
+                log.error("Error "+ex.getMessage());
+                log.error("Rastreo "+ex.getStackTrace());
+                return "Ocurrio un error con la base de datos";
+            }
+
+        }else{
+            return "Departamento "+departamento.getId()+" no se encontró";
+
+        }
+
 
     }
 }
