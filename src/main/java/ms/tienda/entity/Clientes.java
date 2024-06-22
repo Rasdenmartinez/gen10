705b36a3ab23e4e779b9b482a558aa2175f32e4b
pixelsd <alexxag7jj@gmail.com>
2024-06-22 15:58:42 -0600

    aggregation of entities and functions, employee and customers

--- src/main/java/ms/tienda/entity/Clientes.java
+++ src/main/java/ms/tienda/entity/Clientes.java
@@ -1,7 +1,13 @@
 package ms.tienda.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "clientes")
 public class Clientes {
@@ -25,64 +31,4 @@ public class Clientes {
     @Column (name = "telefono")
     private String telefono;
 
-    public Clientes(){
-        //constructor vacio
-    }
-
-    public Clientes(Long Id, String nombre, String apellido, String direccion, String mail, String telefono) {
-        this.Id = Id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.direccion = direccion;
-        this.mail = mail;
-        this.telefono = telefono;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long Id) {
-        this.Id = Id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
--- src/main/java/ms/tienda/entity/Empleados.java
+++ src/main/java/ms/tienda/entity/Empleados.java
@@ -1,8 +1,13 @@
 package ms.tienda.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "empleados")
 public class Empleados {
@@ -30,64 +35,4 @@ public class Empleados {
     @Column(name = "fecha_contratacion")
     private String fechaContratacion;
 
-    public Empleados() {
-        //constructor vacio
-    }
-
-    public Empleados(Long idEmpleado, String nombre, String apellido, String puesto, Double salario, String fechaContratacion) {
-        this.idEmpleado = idEmpleado;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.puesto = puesto;
-        this.salario = salario;
-        this.fechaContratacion = fechaContratacion;
-    }
-
-    public Long getIdEmpleado() {
-        return idEmpleado;
-    }
-
-    public void setIdEmpleado(Long idEmpleado) {
-        this.idEmpleado = idEmpleado;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getPuesto() {
-        return puesto;
-    }
-
-    public void setPuesto(String puesto) {
-        this.puesto = puesto;
-    }
-
-    public Double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(Double salario) {
-        this.salario = salario;
-    }
-
-    public String getFechaContratacion() {
-        return fechaContratacion;
-    }
-
-    public void setFechaContratacion(String fechaContratacion) {
-        this.fechaContratacion = fechaContratacion;
-    }
 }
