a47296cbc7698115556a1aa22751360812907c79
Jorge Luis <77176041+dxdiag41@users.noreply.github.com>
2024-07-04 23:34:48 -0600

    reset

--- tienda/tienda/src/main/java/ms/tienda/repository/EmpleadosRepository.java
+++
@@ -1,8 +0,0 @@
-package ms.tienda.repository;
-
-import ms.tienda.entity.Empleados;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadosRepository extends JpaRepository<Empleados, Long> {
-    //manipulacion del CRUD
-}
